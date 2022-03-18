b5b0875a39fe35c1594d8bfbcfec1e43005e4e3f
jesiallen <justjesi2010@gmail.com>
2022-03-18 23:13:11 +0000

    Completed simple JavaScript Excuse Generator with randomization.

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoDid = who[Math.floor(Math.random() * who.length)];
+  let whatAction = action[Math.floor(Math.random() * action.length)];
+  let toWhat = what[Math.floor(Math.random() * what.length)];
+  let happened = when[Math.floor(Math.random() * when.length)];
+
+  document.querySelector(
+    "#excuse"
+  ).innerHTML = `${whoDid} ${whatAction} ${toWhat} ${happened}`;
 };
